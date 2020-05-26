9f3eacef798b5f10fcc08519c64906d99c806dc1
Domenico Miranda <mirandadomenico95@gmail.com>
2020-05-26 18:58:02 +0200

    prova
    cancellato commento

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
+  //aaaaa
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TeamUp',
