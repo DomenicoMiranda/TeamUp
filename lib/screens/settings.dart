80642416870a8d6b504a7d53582d29415f717294
domenicomiranda <mirandadomenico95@gmail.com>
2020-06-08 14:53:58 +0200

    update wit signout

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:teamup/database/auth.dart';
+import 'package:teamup/screens/home.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -7,63 +9,63 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-
-  
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
-      appBar:
-      AppBar(
-        title: Text("Profilo"),
-        centerTitle: true,
-      ),
-      body: wrapper()
-      );
+        appBar: AppBar(
+          title: Text("Profilo"),
+          centerTitle: true,
+        ),
+        body: wrapper());
   }
 
-wrapper(){
-      
+  wrapper() {
+    if (FirebaseAuth.instance.currentUser() != null) {
+      print("Logged");
+      return loggedIn();
+    } else {
+      print("not logged");
+      return notLoggedIn();
+    }
+  }
 
-  if(FirebaseAuth.instance.currentUser() != null){
-    print("Logged");
-    return loggedIn();
-  }else
-  print("not logged");
-  return notLoggedIn();
-
-}
-
-notLoggedIn(){
-
+  notLoggedIn() {
     return SingleChildScrollView(
-      child:
-      Center(
-        child: Column(
-          children: [
-            Container(
-              height: 200,
-              width: MediaQuery.of(context).size.width/2,
-              child:
-                Text("Per poter accedere alle funzionalità dell'app devi essere registrato",overflow: TextOverflow.visible, textAlign: TextAlign.center,)
-            ),
-            RaisedButton(
-              onPressed: null, 
-              child: Container(
-                height: 30,
-                width: MediaQuery.of(context).size.width/2,
-                child: Text("LOGIN"),))
-          ]
+        child: Center(
+      child: Column(children: [
+        Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width / 2,
+            child: Text(
+              "Per poter accedere alle funzionalità dell'app devi essere registrato",
+              overflow: TextOverflow.visible,
+              textAlign: TextAlign.center,
+            )),
+        RaisedButton(
+            onPressed: null,
+            child: Container(
+              height: 30,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Text("LOGIN"),
+            ))
+      ]),
+    ));
+  }
+
+  loggedIn() {
+    return SingleChildScrollView(
+        child: Center(
+      child: RaisedButton(
+        onPressed: () async {
+          await AuthService().signOut();
+          return new Homepage();
+        },
+        child: Container(
+          height: 30,
+          width: MediaQuery.of(context).size.width / 2,
+          child: Text("LOGOUT"),
         ),
-      )
-
-    );
-}
-
-loggedIn(){
-
-
-}
-  
+      ),
+    ));
+  }
 }
