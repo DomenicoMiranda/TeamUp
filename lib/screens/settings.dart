b9e27e063a40cd3c3168bf6a36a4d4ce5b93b930
domenicomiranda <mirandadomenico95@gmail.com>
2020-06-08 12:12:25 +0200

    added wrapper

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -6,10 +7,63 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:Text("Impostazioni")
-    );
+
+
+    return Scaffold(
+      appBar:
+      AppBar(
+        title: Text("Profilo"),
+        centerTitle: true,
+      ),
+      body: wrapper()
+      );
   }
+
+wrapper(){
+      
+
+  if(FirebaseAuth.instance.currentUser() != null){
+    print("Logged");
+    return loggedIn();
+  }else
+  print("not logged");
+  return notLoggedIn();
+
+}
+
+notLoggedIn(){
+
+    return SingleChildScrollView(
+      child:
+      Center(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: MediaQuery.of(context).size.width/2,
+              child:
+                Text("Per poter accedere alle funzionalità dell'app devi essere registrato",overflow: TextOverflow.visible, textAlign: TextAlign.center,)
+            ),
+            RaisedButton(
+              onPressed: null, 
+              child: Container(
+                height: 30,
+                width: MediaQuery.of(context).size.width/2,
+                child: Text("LOGIN"),))
+          ]
+        ),
+      )
+
+    );
+}
+
+loggedIn(){
+
+
+}
+  
 }
