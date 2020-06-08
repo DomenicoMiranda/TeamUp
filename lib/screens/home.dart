3a3b03b454f695ec10663d6632de2703522e9acb
domenicomiranda <mirandadomenico95@gmail.com>
2020-06-08 10:41:11 +0200

    added reset password

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:teamup/authentication/login.dart';
+import 'package:teamup/authentication/resetpassword.dart';
 
 class Homepage extends StatefulWidget {
   @override
@@ -9,10 +11,32 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-                    child:
-                Text(" HomePage()"),
-               
-
+      child: Center(
+        child: 
+        Column(children: [
+        FlatButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Login()),
+              );
+            },
+            child: Container(
+              height: 20,
+              width: 60,
+              color: Colors.red)),
+              FlatButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Resetpassword()),
+              );
+            },
+            child: Container(
+              height: 20,
+              width: 60,
+              color: Colors.black)),])
+      ),
     );
   }
 }
