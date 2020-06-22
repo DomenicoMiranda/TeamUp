8325f4cac4983dc1059eb1b1b514836b278afb6d
salvatoreanastasia <s.anastasia1@studenti.uniba.it>
2020-06-22 20:35:17 +0200

    Added category filter and scrolling on TabBar // HOME

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,25 +13,8 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        centerTitle: true,
-          actions: <Widget>[
-            // action button
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CreateProject()),
-                );
-              },
-            ),
-          ]
-      ),
       body: Column(
         children: <Widget>[
-          CategorySelector(),
           Expanded(
             child: Container(
               decoration: BoxDecoration(
--- lib/widgets/cardView.dart
+++ lib/widgets/cardView.dart
@@ -1,135 +1,331 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:teamup/database/auth.dart';
+import 'package:teamup/screens/create_project.dart';
 
 class CardView extends StatefulWidget {
-
   @override
   _CardViewState createState() => _CardViewState();
 }
 
 class _CardViewState extends State<CardView> {
-  DocumentSnapshot document;
 
-  String category;
+  List<Widget> containersProjects = [
 
+    buildAllCategories(),
+    buildMusica(),
+    buildArte(),
+    buildSport(),
+    buildCinema(),
+    buildBusiness(),
 
-  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
-    return Container(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 200.0,
-            //margin: new EdgeInsets.only(right: 46.0),
-            decoration: new BoxDecoration(
-              //color: Colors.blueGrey.shade200,
-              color: Colors.blueGrey.shade200,
-              shape: BoxShape.rectangle,
-              borderRadius: new BorderRadius.circular(30.0),
-              boxShadow: <BoxShadow>[
-                new BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 10.0,
-                  offset: new Offset(0.0, 10.0),
-                ),
-              ],
-            ),
+  ];
 
-            //Circle Avatar
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      CircleAvatar(
-                        radius: 40,
-                        backgroundImage: NetworkImage(
-                            "http://49.231.30.115/emrcleft/assets/images/avatars/avatar2_big@2x.png"),
-                      ),
-
-                      SizedBox(width: 20),
-                      //titolo e material button
-                      Expanded(
-                        child: Column(
-                          children: <Widget>[
-                            Text(document['name'],
-                                style: TextStyle(
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white70)),
-                            MaterialButton(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              minWidth: double.infinity,
-                              height: 32,
-                              color: Colors.blue.shade500,
-                              onPressed: () {},
-                              child: Text("Candidami!",
-                                  style: TextStyle(color: Colors.white)),
-                            ),
-                          ],),
-                      )
-                    ],
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: DefaultTabController(
+        length: 6,
+        child: new Scaffold(
+            appBar: new AppBar(
+              title: const Text('Home'),
+              centerTitle: true,
+                actions: <Widget>[
+                  // action button
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => CreateProject()),
+                      );
+                    },
                   ),
-
-                  SizedBox(height: 3),
-
-                  //descrizione Card
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: <Widget>[
-                      ],
-                    ),
-                  ),
-
-                  SizedBox(height: 40),
-
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsetsDirectional.only(start: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text("Num. Posti disponibili", style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white70)),
-                          Text(document['maxTeammate'].toString(),
-                              style: TextStyle(fontSize: 30,
-                                  fontWeight: FontWeight.w300,
-                                  color: Colors.white54)),
-                        ],
-                      ),
-                    ),
-                  ),
-
                 ],
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(20.0),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: TabBar(
+                        isScrollable: true, tabs: [
+                      new Container(
+                          width: MediaQuery.of(context).size.width / 2,
+                          child: new Tab(text: 'Tutte')
+                      ),
+                      new Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: new Tab(text: 'Musica'),
+                      ),
+                      new Container(
+                          width: MediaQuery.of(context).size.width / 2,
+                          child: new Tab(text: 'Arte')
+                      ),
+                      new Container(
+                          width: MediaQuery.of(context).size.width / 2,
+                          child: new Tab(text: 'Sport')
+                      ),
+                      new Container(
+                          width: MediaQuery.of(context).size.width / 2,
+                          child: new Tab(text: 'Cinema')
+                      ),
+                      new Container(
+                          width: MediaQuery.of(context).size.width / 2,
+                          child: new Tab(text: 'Business')
+                      ),
+
+                    ]),
+                  ),
+                ),
               ),
             ),
+            /*body: new TabBarView(
+                  children: [
+                    new ListView(
+                      children: list,
+                    ),
+                    new ListView(
+                      children: list,
+                    ),
+                  ],
+                ),*/
+            body: TabBarView(
+              children: containersProjects,
+            )
+
+        ),
+      ),
+    );
+  }
+}
+
+//query per recuperare tutte le info da Firebase
+
+@override
+Widget buildAllCategories() {
+  return Container(
+    child: StreamBuilder(
+        stream: Firestore.instance
+            .collection('projects')
+            .where("stato", isEqualTo: "0")
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Text('Loading');
+          return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) => _buildListItem(context, snapshot.data.documents[index])
+          );
+        }
+    ),
+  );
+}
+
+
+@override
+Widget buildArte() {
+  AuthService authUser = new AuthService();
+  return Container(
+    child: StreamBuilder(
+        stream: Firestore.instance
+            .collection('projects')
+            .where("stato", isEqualTo: "0")
+            .where("category", isEqualTo: "Arte")
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Text('Loading');
+          return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) => _buildListItem(context, snapshot.data.documents[index])
+          );
+        }
+    ),
+  );
+}
+
+@override
+Widget buildMusica() {
+  AuthService authUser = new AuthService();
+  return Container(
+    child: StreamBuilder(
+        stream: Firestore.instance
+            .collection('projects')
+            .where("stato", isEqualTo: "0")
+            .where("category", isEqualTo: "Musica")
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Text('Loading');
+          return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) => _buildListItem(context, snapshot.data.documents[index])
+          );
+        }
+    ),
+  );
+}
+
+@override
+Widget buildSport() {
+  AuthService authUser = new AuthService();
+  return Container(
+    child: StreamBuilder(
+        stream: Firestore.instance
+            .collection('projects')
+            .where("stato", isEqualTo: "0")
+            .where("category", isEqualTo: "Sport")
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Text('Loading');
+          return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) => _buildListItem(context, snapshot.data.documents[index])
+          );
+        }
+    ),
+  );
+}
+
+@override
+Widget buildCinema() {
+  AuthService authUser = new AuthService();
+  return Container(
+    child: StreamBuilder(
+        stream: Firestore.instance
+            .collection('projects')
+            .where("stato", isEqualTo: "0")
+            .where("category", isEqualTo: "Cinema")
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Text('Loading');
+          return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) => _buildListItem(context, snapshot.data.documents[index])
+          );
+        }
+    ),
+  );
+}
+
+@override
+Widget buildBusiness() {
+  AuthService authUser = new AuthService();
+  return Container(
+    child: StreamBuilder(
+        stream: Firestore.instance
+            .collection('projects')
+            .where("stato", isEqualTo: "0")
+            .where("category", isEqualTo: "Business")
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Text('Loading');
+          return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) => _buildListItem(context, snapshot.data.documents[index])
+          );
+        }
+    ),
+  );
+}
+
+Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+  return Container(
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 200.0,
+        //margin: new EdgeInsets.only(right: 46.0),
+        decoration: new BoxDecoration(
+          //color: Colors.blueGrey.shade200,
+          color: Colors.blueGrey.shade200,
+          shape: BoxShape.rectangle,
+          borderRadius: new BorderRadius.circular(30.0),
+          boxShadow: <BoxShadow>[
+            new BoxShadow(
+              color: Colors.black12,
+              blurRadius: 10.0,
+              offset: new Offset(0.0, 10.0),
+            ),
+          ],
+        ),
+
+        //Circle Avatar
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: NetworkImage(
+                        "http://49.231.30.115/emrcleft/assets/images/avatars/avatar2_big@2x.png"),
+                  ),
+
+                  SizedBox(width: 20),
+                  //titolo e material button
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        Text(document['name'],
+                            style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white70)),
+                        MaterialButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          minWidth: double.infinity,
+                          height: 32,
+                          color: Colors.blue.shade500,
+                          onPressed: () {},
+                          child: Text("Candidami!",
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ],),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 3),
+
+              //descrizione Card
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 40),
+
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.only(start: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("Num. Posti disponibili", style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white70)),
+                      Text(document['maxTeammate'].toString(),
+                          style: TextStyle(fontSize: 30,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white54)),
+                    ],
+                  ),
+                ),
+              ),
+
+            ],
           ),
         ),
-      );
+      ),
+    ),
+  );
 
 
-  }
-
-    @override
-    Widget build(BuildContext context) {
-      return Container(
-        child: StreamBuilder(
-            stream: Firestore.instance
-                .collection('projects')
-                .snapshots(),
-            builder: (context, snapshot) {
-             if (!snapshot.hasData) return const Text('Loading');
-              return ListView.builder(
-                itemCount: snapshot.data.documents.length,
-                itemBuilder: (context, index) => _buildListItem(context, snapshot.data.documents[index])
-              );
-            }
-        ),
-      );
-    }
 }
+
